e82a3d7207c850ffb3e76ccf82f0e53de344b68e
Ceilican <bruno.wp@gmail.com>
2013-02-01 14:35:31 -0200

    Remove backups

--- Backup/0.1 (with iFrame)/injector.js
+++
@@ -1,7 +0,0 @@
-function injectJS(link) {
-  var scr = document.createElement('script');
-  scr.type="text/javascript";
-  scr.src=link;
-  document.getElementsByTagName('head')[0].appendChild(scr);
-}
-injectJS(chrome.extension.getURL('show_and_hide.js'));
