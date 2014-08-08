7d860105fe459834ad73ddfba373f64bf881eb99
RJ Marsan <rjmarsan@google.com>
2014-08-08 13:16:05 -0700

    Yes. options page is working again.

--- Source/options.js
+++ Source/options.js
@@ -7,15 +7,14 @@ function e(id) {
 
 $(function() {	
   var languageLoaded = function(){
-    loadStorageIntoCache(function() {
-      set_languages();
-      restore_options();
-      showCSSExample();
+    loadStorageAndUpdate(function(data) {
+      initUi();
+      updateUi(data);
     });
   };
   google.load("language", "1", {callback: languageLoaded});
 
-  document.addEventListener("DOMContentLoaded", function () {
+  function setupListeners() {
     e("addTranslationBtn").addEventListener("click", createPattern);
     e("translatedWordStyle").addEventListener("keyup", showCSSExample);
     e("minimumSourceWordLength").addEventListener("blur", save_minimumSourceWordLength);
@@ -23,7 +22,32 @@ $(function() {
     e("blacklist").addEventListener("blur", save_blacklist);
     e("userDefinedTranslations").addEventListener("blur", save_userDefinedTranslations);
     e("userBlacklistedWords").addEventListener("blur", save_userBlacklistedWords);
-  });
+  }
+
+  function initUi() {
+    setLanguages();
+    setupListeners();
+  }
+
+  //Sets Languages
+  function setLanguages(){
+    var languages = google.language.Languages
+    var targetLanguageOptions = " "
+    for(var language in languages) {
+      var name = language.substring(0, 1) + language.substring(1).toLowerCase().replace('_', ' - ');
+      targetLanguageOptions += '<option value="' + languages[language] + '">' + name + '</option>'
+    }
+    e("sourceLanguage").innerHTML = targetLanguageOptions;
+    e("targetLanguage").innerHTML = targetLanguageOptions;
+  }
+
+
+  function updateUi(data) {
+      console.log("Updating UI");
+      restoreOptions(data);
+      restorePatterns(data);
+      showCSSExample();
+  }
 
   /***
   * Text: String, what you'd like it to say
@@ -66,87 +90,8 @@ $(function() {
     e("resultSpan").innerText = synonyms[num];
   }
   
-  //Sets Languages
-  function set_languages(){
-    var languages = google.language.Languages
-    var targetLanguageOptions = " "
-    for(var language in languages) {
-      var name = language.substring(0, 1) + language.substring(1).toLowerCase().replace('_', ' - ');
-      targetLanguageOptions += '<option value="' + languages[language] + '">' + name + '</option>'
-    }
-    e("sourceLanguage").innerHTML = targetLanguageOptions;
-    e("targetLanguage").innerHTML = targetLanguageOptions;
-  }
-
-  function S(key) { return cachedStorage[key]; } 
-
-  function loadStorageIntoCache(callback) {
-    storage.get(null, function(data) {
-      cachedStorage = data;
-      if (!!callback) {
-        callback(data);
-      } 
-    });
-  }
-
-  function saveBulk(data, message) {
-    storage.set(data, function() {
-      statusDefault(message);
-      loadStorageIntoCache();
-      restorePatterns();
-    });
-  }
-
-  function save(id, message) {
-    var elem = e(id);
-    
-    var v;
-    if (elem.tagName.toLowerCase() == "select") {
-      v = elem.children[elem.selectedIndex].value;    
-    }
-    else {
-      v = elem.value; 
-    }
-
-    if (cachedStorage[id] != v) {
-      saveBulk({id: v}, message);
-    }
-  }
-
-  // Save to localStorage.
-  function save_userDefinedTranslations() {
-    try { 
-      JSON.parse(e("userDefinedTranslations").value);
-      save("userDefinedTranslations", "User-defined translations saved");
-    } 
-    catch(e) {
-      console.debug(S("userDefinedTranslations"))
-      status('Your user-defined translations are badly specified and therefore will not be used. \
-              Please provide your user-defined translations according to the following format: \
-              \n\n {"word1":"translation1", "word2":"translation2", "word3":"translation3", "word4":"translation4"}', 9000, 600, "error");
-    }
-  }
 
 
-  function save_minimumSourceWordLength() {
-    // ToDo: Implement validation
-    save("minimumSourceWordLength", "Minimum word length saved");
-  }
-
-  function save_translatedWordStyle() {
-    // ToDo: Implement validation
-    save("translatedWordStyle","Translated word style saved");
-  }
-
-  function save_blacklist() {
-    // ToDo: Implement validation
-    save("blacklist", "Blacklist saved");
-  }
-
-  function save_userBlacklistedWords() {
-    // ToDo: Implement validation
-    save("userBlacklistedWords", "Blacklisted words saved");
-  }
 
   
 
@@ -170,110 +115,205 @@ $(function() {
                  prb[1],
                  false
                 ]);
-    save("savedPatterns", JSON.stringify(pttrns));
+    saveBulk({"savedPatterns": JSON.stringify(pttrns)});
   }
 
 
-  function restorePatterns(){
+  function restorePatterns(data){
     e("savedTranslationPatterns").innerHTML = "";
-    var pttrns = JSON.parse(S("savedPatterns")),
-        html = "";
+    var patterns = JSON.parse(data["savedPatterns"]);
+    var patternsElem = $("#savedTranslationPatterns").html("");
 
-    for(var i in pttrns){
-      html += "<p class='alert alert-"+((pttrns[i][3] && !!S("activation")) ? "success" : "nothing")+" tPattern'> \
+    $.each(patterns, function(i, pattern) {
+      var listElem = $("<p class='alert alert-"+((pattern[3] && !!data["activation"]) ? "success" : "nothing")+" tPattern'> \
                 Translate \
-                <span class='label label-info'>"+pttrns[i][2]+"%</span> \
+                <span class='label label-info'>"+pattern[2]+"%</span> \
                 of all \
-                <span class='label label-info'>"+pttrns[i][0][1]+"</span> \
+                <span class='label label-info'>"+pattern[0][1]+"</span> \
                 words into \
-                <span class='label label-info'>"+pttrns[i][1][1]+"</span> \
+                <span class='label label-info'>"+pattern[1][1]+"</span> \
                 <button class='btn btn-danger pull-right deletePattern'>Delete</button>\
                 <input type='hidden' value='"+i+"' />\
-              </p>";
-    }
+              </p>");
+      listElem.click(function() {
+        activatePattern(i, patterns);
+      });
+      listElem.find('.deletePattern').click(function(e) {
+        deletePattern(i, patterns, e);
+      });
+      patternsElem.append(listElem);
+    });
 
-    html += "<p class='alert alert-"+((!S("activation")) ? "success" : "nothing")+" tPattern'> \
+    var nonElem = $("<p class='alert alert-"+((!data["activation"]) ? "success" : "nothing")+" tPattern'> \
               Do not translate \
               <input type='hidden' value='-1' \
-            </p>";
-
-    e("savedTranslationPatterns").innerHTML = html;
-    var pttrns = document.getElementsByClassName("tPattern");
-    for(var i=0; i<pttrns.length; i++){ pttrns[i].addEventListener("click", activatePattern); }
-    var delPattern = document.getElementsByClassName("deletePattern");
-    for(var i=0; i<delPattern.length; i++){ delPattern[i].addEventListener("click", deletePattern); }
+            </p>");
+    nonElem.click(function() {
+      activatePattern(-1, data);
+    });
+    patternsElem.append(nonElem);
   }
 
 
-  function deletePattern(c){
-    c.stopPropagation();
-    var _id = this.parentNode.getElementsByTagName("input")[0].value,
-        pttrns = JSON.parse(S("savedPatterns")),
+  function deletePattern(index, patterns, e){
+    e.stopPropagation();
+    var _id = index,
         moveTrue = false; // Are you deleting the active pattern?
     
-    if(pttrns.length > 1){
-      if(pttrns[_id][3]){ moveTrue = true; }
-      pttrns.splice(_id,1);
-      if(moveTrue){ pttrns[0][3] = true; }
-      save("savedPatterns", JSON.stringify(pttrns));
+    if(patterns.length > 1){
+      if(patterns[_id][3]) { 
+        moveTrue = true;
+      }
+      patterns.splice(_id,1);
+      if(moveTrue){
+        patterns[0][3] = true;
+        activatePattern(0, patterns);
+      } else {
+        saveBulk({"savedPatterns": JSON.stringify(patterns)});
+      }
     }
   }
 
-  function activatePattern(){
-    var _id = this.getElementsByTagName("input")[0].value,
-        pttrns = JSON.parse(S("savedPatterns"));
+  function activatePattern(index, patterns){
+    var _id = index;
 
-    toSave = {};
+    var toSave = {};
     if (_id == -1) {
       toSave["activation"] = false;
     } else {
       toSave["activation"] = true;
 
-      var selectedPattern = pttrns[_id];      
+      var selectedPattern = patterns[_id];      
       
       toSave["sourceLanguage"] = selectedPattern[0][0];
       toSave["targetLanguage"] = selectedPattern[1][0];
       toSave["translationProbability"] = selectedPattern[2];
     }
 
-    for(var i in pttrns){ pttrns[i][3] = (i == _id ? true : false); }
-    toSave["savedPatterns"] = JSON.stringify(pttrns);
+    for(var i in patterns){ patterns[i][3] = (i == _id ? true : false); }
+    toSave["savedPatterns"] = JSON.stringify(patterns);
     saveBulk(toSave);
   }
 
 
-  function restore_options() {
+  function restoreOptions(data) {
     var options = ["sourceLanguage", "targetLanguage", "translationProbability", 
                "minimumSourceWordLength", "translatedWordStyle", "blacklist",
                "userDefinedTranslations", "userBlacklistedWords"];
 
     for (index in options) {
-      console.log("Restoring:", options[index]);
-      restore(options[index]);
+      restore(options[index], data);
     }
   }
 
-  function restore(option) {
-    console.debug("restore(" + option + ")");
-
+  function restore(option, data) {
     var elem = e(option);
     var type = elem.tagName.toLowerCase();
 
-    console.debug("Value for " + option + " in localStorage is: " + S(option));
+    console.debug("Restore " + option + " to: " + data[option]);
 
     if (type == "select") {
       for (var i = 0; i < elem.children.length; i++) {
         var child = elem.children[i];
-        if (child.value == S(option)) {
+        if (child.value == data[option]) {
           child.selected = "true";
           break;
         }
       }
-    }
-    else {
-      elem.value = S(option);
+    } else {
+      elem.value = data[option];
     }   
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /** Storage **/
+  function S(key) { return cachedStorage[key]; } 
+
+  function loadStorageAndUpdate(callback) {
+    storage.get(null, function(data) {
+      console.log("Loaded storage");
+      cachedStorage = data;
+      if (!!callback) {
+        callback(data);
+      } 
+    });
+  }
+
+  function saveBulk(data, message) {
+    storage.set(data, function() {
+      statusDefault(message);
+      loadStorageAndUpdate(function(data) {
+        updateUi(data);
+      });
+    });
+  }
+
+  function save(id, message) {
+    var elem = e(id);
+    
+    var v;
+    if (elem.tagName.toLowerCase() == "select") {
+      v = elem.children[elem.selectedIndex].value;    
+    }
+    else {
+      v = elem.value; 
+    }
+    console.log("Saving "+id+" as "+v);
+    if (cachedStorage[id] != v) {
+      var map = {};
+      map[id] = v;
+      saveBulk(map, message);
+    }
+  }
+
+  // Save to localStorage.
+  function save_userDefinedTranslations() {
+    try { 
+      JSON.parse(e("userDefinedTranslations").value);
+      save("userDefinedTranslations", "User-defined translations saved");
+    } 
+    catch(e) {
+      console.debug(S("userDefinedTranslations"))
+      status('Your user-defined translations are badly specified and therefore will not be used. \
+              Please provide your user-defined translations according to the following format: \
+              \n\n {"word1":"translation1", "word2":"translation2", "word3":"translation3", "word4":"translation4"}', 9000, 600, "error");
+    }
+  }
+
+  function save_minimumSourceWordLength() {
+    // ToDo: Implement validation
+    save("minimumSourceWordLength", "Minimum word length saved");
+  }
+
+  function save_translatedWordStyle() {
+    // ToDo: Implement validation
+    save("translatedWordStyle","Translated word style saved");
+  }
+
+  function save_blacklist() {
+    // ToDo: Implement validation
+    save("blacklist", "Blacklist saved");
+  }
+
+  function save_userBlacklistedWords() {
+    // ToDo: Implement validation
+    save("userBlacklistedWords", "Blacklisted words saved");
+  }
+
 
   google_analytics('UA-1471148-14');  
 });
