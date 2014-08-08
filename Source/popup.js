7057345869059692df115a6b2088912b426928b8
RJ Marsan <rjmarsan@google.com>
2014-08-08 13:19:03 -0700

    Options button in popup

--- Source/popup.js
+++ Source/popup.js
@@ -2,7 +2,12 @@ function toggleWords() {
   chrome.tabs.executeScript(null, {code:"__mindtheword.toggleAllElements();"});
   window.close();
 }
+function options() {
+  chrome.tabs.create({url:chrome.extension.getURL("options.html")});
+  window.close();
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('languageToggle').addEventListener('click', toggleWords);
+  document.getElementById('optionsButton').addEventListener('click', options);
 });
