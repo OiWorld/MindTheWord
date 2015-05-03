bfe36635c5272539054c5001a78089813699d99d
omesh <pandita.omesh@gmail.com>
2015-05-04 01:00:10 +0530

    Cleanup code.

--- Source/popup.js
+++ Source/popup.js
@@ -1,36 +1,36 @@
 var app = angular.module('PopupApp', []);
-app.controller('PopupController', ['$scope', function($scope) {
-	$scope.toggleWords = function() {
-		chrome.tabs.executeScript(null, {code:"__mindtheword.toggleAllElements();"}, function() {
-			$scope.updateData();
-		});
-	};
+app.controller('PopupController', ['$scope', function ($scope) {
+    $scope.toggleWords = function () {
+        chrome.tabs.executeScript(null, {code: "__mindtheword.toggleAllElements();"}, function () {
+            $scope.updateData();
+        });
+    };
 
-	$scope.toggleEnabled = function() {
-	    chrome.storage.sync.get("activation", function(data) {
-	    	chrome.storage.sync.set({activation: !data.activation}, function() {
-				chrome.tabs.executeScript(null, {code:"window.location.reload();"});
-				window.close();			
-	    	});
-    	});
-	};
+    $scope.toggleEnabled = function () {
+        chrome.storage.sync.get("activation", function (data) {
+            chrome.storage.sync.set({activation: !data.activation}, function () {
+                chrome.tabs.executeScript(null, {code: "window.location.reload();"});
+                window.close();
+            });
+        });
+    };
 
-	$scope.options = function() {
-		chrome.tabs.create({url:chrome.extension.getURL("options.html")});
-		window.close();
-	};
+    $scope.options = function () {
+        chrome.tabs.create({url: chrome.extension.getURL("options.html")});
+        window.close();
+    };
 
-	$scope.updateData = function() {
-		chrome.tabs.executeScript(null, {code:"__mindtheword.isTranslated()"}, function(translated) {
-			$scope.$apply(function() {
-				$scope.toggledOn = translated[0];
-			});
-		});
-	    chrome.storage.sync.get(null, function(data) {
-	    	$scope.$apply(function() {
-	    		$scope.data = data;
-			});
-    	});
-	};
-	$scope.updateData();
+    $scope.updateData = function () {
+        chrome.tabs.executeScript(null, {code: "__mindtheword.isTranslated()"}, function (translated) {
+            $scope.$apply(function () {
+                $scope.toggledOn = translated[0];
+            });
+        });
+        chrome.storage.sync.get(null, function (data) {
+            $scope.$apply(function () {
+                $scope.data = data;
+            });
+        });
+    };
+    $scope.updateData();
 }]);
