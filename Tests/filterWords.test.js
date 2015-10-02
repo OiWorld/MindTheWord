e98bd58bd4939bff5fde03f6296e3f1b67acb910
Chris Harding <chrish@cantab.net>
2015-10-02 01:19:33 +0100

    Test for user defined filter now working correctly.

--- Tests/filterWords.test.js
+++ Tests/filterWords.test.js
@@ -9,25 +9,23 @@ describe('filterSourceWordsPreferUserDefined function', function() {
 
       var wordList = JSON.parse(fs.read('Tests/data/allWords.json'));
       var correctFilteredWordList = JSON.parse(fs.read('Tests/data/filteredWords.json'));
-      var userDefinedWords = JSON.parse(fs.read('Tests/data/userDefinedWords.json'));
+      var userDefinedTranslations = JSON.parse(fs.read('Tests/data/userDefinedTranslations.json'));
 
       // filter word list
       page.injectJs('Source/MindTheWord.js');
-      var filteredWordList = page.evaluate(function(wordList, userDefinedWords) {
+      var filteredWordList = page.evaluate(function(wordList, userDefinedTranslations) {
 
         var countedWords = wordList;
         var translationProbability = 15.0;
         var minimumSourceWordLength = 1;
         var userBlacklistedWords = "(this|that)";
-        return filterSourceWordsPreferUserDefined(countedWords, translationProbability, userDefinedWords);
+        return filterSourceWordsPreferUserDefined(countedWords, translationProbability, userDefinedTranslations);
 
-      }, wordList, userDefinedWords);
-
-      console.log(Object(filteredWordList));
+      }, wordList, userDefinedTranslations);
 
       // check equality
       page.evaluate(function(a, b) {
-        //chai.expect(a).to.deep.equal(b);
+        chai.expect(a).to.deep.equal(b);
       }, filteredWordList, correctFilteredWordList);
 
       done();
