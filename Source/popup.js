c9ede45b1788134a8dc1acb180fa54ccbe577e7b
srikar.ch <srikar.0896@gmail.com>
2016-03-08 13:44:38 +0530

    Update popup.js
    improved javascript style according to Google style guide

--- Source/popup.js
+++ Source/popup.js
@@ -1,36 +1,38 @@
 var app = angular.module('PopupApp', []);
-app.controller('PopupController', ['$scope', function($scope) {
+app.controller('PopupController', ['$scope', scopefunction($scope)]);
+function scopefunction($scope) {
   $scope.toggleWords = function() {
     chrome.tabs.executeScript(null, {code: '__mindtheword.toggleAllElements();'}, function() {
       $scope.updateData();
-    });
-  };
-
+		});
+	};
   $scope.toggleEnabled = function() {
-    chrome.storage.local.get('activation', function(data) {
-      chrome.storage.local.set({activation: !data.activation}, function() {
+    chrome.storage.local.get('activation',active(data));
+  };
+ function active(data) {
+      chrome.storage.local.set({activation: !data.activation}, exec());
+	  }
+  function exec(){
         chrome.tabs.executeScript(null, {code: 'window.location.reload();'});
         window.close();
-      });
-    });
-  };
-
+      }
   $scope.options = function() {
     chrome.tabs.create({url: chrome.extension.getURL('options.html')});
     window.close();
   };
-
   $scope.updateData = function() {
-    chrome.tabs.executeScript(null, {code: '__mindtheword.isTranslated()'}, function(translated) {
-      $scope.$apply(function() {
-        $scope.toggledOn = translated[0];
-      });
-    });
-    chrome.storage.local.get(null, function(data) {
+    chrome.tabs.executeScript(null, {code: '__mindtheword.isTranslated()'},wtranslate(translated));
+    chrome.storage.local.get(null, getdata(data));
+  };
+  function getdata(data){
       $scope.$apply(function() {
         $scope.data = data;
       });
-    });
-  };
+    }
+  function wtranslate(translated){
+      $scope.$apply(function() {
+        $scope.toggledOn = translated[0];
+      });
+    }
   $scope.updateData();
-}]);
+}
